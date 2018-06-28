89dcafd61e21df66137ab30db5d3545a639b40de
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2018-06-28 16:56:52 -0700

    employee route added

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -3,10 +3,12 @@ const bodyParser = require('body-parser');
 
 // object destructuring
 const { mongoose } = require('./db.js');
+var employeeCtrl = require('./controllers/employeeCtrl.js')
 
 var app = express();
 
 // middleware
 app.use(bodyParser.json());
+app.use('/employees', employeeCtrl);
 
 app.listen(3000, () => console.log('Server started at port 3000!'))
