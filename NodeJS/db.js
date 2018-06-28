f0a5478dcf665fbb15965fa9c0dbb410cc33918a
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2018-06-28 16:21:41 -0700

    MongoDB Connected

---
+++ NodeJS/db.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/CrudDB', function (err) {
+    if (!err) {
+        console.log('MongoDB Connection Successful!')
+    } else {
+        console.log('Error in DB Connection: ' + JSON.stringify(err, undefined, 2));
+    }
+});
+
+module.exports = router;
