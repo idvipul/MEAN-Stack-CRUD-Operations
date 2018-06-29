facff21e1df3ea936dcecf6123bd663d166cda40
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2018-06-28 19:12:44 -0700

    insert employee working

--- NodeJS/controllers/employeeCtrl.js
+++ NodeJS/controllers/employeeCtrl.js
@@ -15,4 +15,25 @@ router.get('/', (req, res) => {
     });
 });
 
+// insert into employees
+router.post('/', (req, res) => {
+    // create an object of employee model class i.e. send JSON data of new employees
+    var emp = new Employee({
+        name: req.body.name,
+        position: req.body.position,
+        option: req.body.option,
+        salary: req.body.salary
+    })
+
+    // insert new record into MongoDB
+    emp.save((err, docs) => {
+        if (!err) {
+            // res.send(docs);
+            res.json(docs);
+        } else {
+            console.log("Error in Employee save: " +JSON.stringify(err, undefined, 2));
+        }
+    });
+});
+
 module.exports = router;
