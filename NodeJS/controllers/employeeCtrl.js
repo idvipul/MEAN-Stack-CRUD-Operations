189a1cf6ad5d0c3347654bae3e7ac42f4fbce69a
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2018-06-28 19:48:41 -0700

    view emp by id working

--- NodeJS/controllers/employeeCtrl.js
+++ NodeJS/controllers/employeeCtrl.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var router = express.Router();
+var objectId = require('mongoose').Types.ObjectId;
 
 // store exported employee from models
 var { Employee } = require('../models/employee');
@@ -17,21 +18,37 @@ router.get('/', (req, res) => {
 
 // insert into employees
 router.post('/', (req, res) => {
-    // create an object of employee model class i.e. send JSON data of new employees
+    // create an object of employee model class i.e. send JSON data of employees
     var emp = new Employee({
         name: req.body.name,
         position: req.body.position,
         option: req.body.option,
         salary: req.body.salary
-    })
+    });
 
     // insert new record into MongoDB
     emp.save((err, docs) => {
         if (!err) {
-            // res.send(docs);
-            res.json(docs);
+            res.send(docs);
         } else {
             console.log("Error in Employee save: " +JSON.stringify(err, undefined, 2));
+        }
+    });
+});
+
+// view employee by id
+router.get('/:id', (req, res) => {
+    // check if id is valid MongoDB id
+    if (!objectId.isValid(req.params.id))
+        // return res.status(400).send(`No record with given id: ${req.params.id}`);
+        return res.status(400).send("No record with given id:" +req.params.id);
+
+    // if valid -- search id
+    Employee.findById(req.params.id, (err, doc) => {
+        if (!err) {
+            res.send(doc);
+        } else {
+            console.log("Error in Retrieving Employee: " +JSON.stringify(err, undefined, 2));
         }
     });
 });
